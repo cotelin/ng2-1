e914544463c700d361fef615a8e2ac910964ccb0
Cotoi <cotoi@Cotois-MBP.home>
2015-12-08 21:13:34 +0000

    set random default like, dislike

--- src/app/article.js
+++ src/app/article.js
@@ -15,8 +15,8 @@ var Article = (function () {
         this.body = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
         this.big_image = 'http://www.iedesign.co.uk/sites/all/themes/IE/logo.svg';
         this.small_image = 'http://www.iedesign.co.uk/sites/all/themes/IE/logo.svg';
-        this.no_of_likes = 0;
-        this.no_of_dislikes = 0;
+        this.no_of_likes = Math.floor((Math.random() * 100) + 1);
+        this.no_of_dislikes = Math.floor((Math.random() * 100) + 1);
     }
     Article.prototype.like = function () {
         this.no_of_likes++;
--- src/app/article.ts
+++ src/app/article.ts
@@ -11,8 +11,8 @@ export class Article{
     body: string = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
     big_image:string        = 'http://www.iedesign.co.uk/sites/all/themes/IE/logo.svg';
     small_image:string      = 'http://www.iedesign.co.uk/sites/all/themes/IE/logo.svg';
-    no_of_likes:number      = 0;
-    no_of_dislikes: number  = 0;
+    no_of_likes:number      = Math.floor((Math.random() * 100) + 1);
+    no_of_dislikes: number  = Math.floor((Math.random() * 100) + 1);
 
     like(){
         this.no_of_likes++;
